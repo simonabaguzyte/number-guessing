a6153b7b3fb0aa8d0c3979b2b4b5ff548f261203
Simona <baguzyte98@gmail.com>
2022-06-15 22:56:01 +0300

    updates

--- number_guessing.py
+++ number_guessing.py
@@ -1,8 +1,11 @@
 from random import randint
 
 
+MIN_VALUE = 1
+MAX_VALUE = 100
+
 print("\n\n*** WELCOME TO THE NUMBER GUESSING PROGRAM! ***")
-print("\nThe program automaticly selects one number from 1 to 100 in integers scale.\
+print(f"\nThe program automaticly selects one number from {MIN_VALUE} to {MAX_VALUE} in integers scale.\
 \nYour task is to guess that number. Do not worry, you are going to get some hints if needed.\
 \nGood luck!\n")
 
@@ -12,27 +15,26 @@ print("\nLet the guessing begin!")
 shouldContinue = True
 while shouldContinue:
 
-    randomNumber = randint(1, 100)
-    print("*Psst, the random number is %s.*" % (randomNumber))
+    randomNumber = randint(MIN_VALUE, MAX_VALUE)
 
     shouldContinue2 = True
     while shouldContinue2:
 
-        guess = int(input("\nEnter your guess (1-100): "))
+        # neveikia, kai rasai raide
+        guess = int(input(f"\nEnter your guess ({MIN_VALUE}-{MAX_VALUE}): "))
 
-        if guess < 1 or guess > 100:
-            print("Your input is not in the scale from 1 to 100.\n")
+        if guess < MIN_VALUE or guess > MAX_VALUE:
+            print(f"Your input is not in the scale from {MIN_VALUE} to {MAX_VALUE}.\n")
             print("Please, check your input and try again.")
 
         elif guess == randomNumber:
-            print("BRAVO!!! You have guessed it! The number was %s." % randomNumber)
-            choice = input("\n\nWould you like to play again (Y/N)?: ")
+            print(f"BRAVO!!! You have guessed it! The number was {randomNumber}.")
+            choice = input("\n\nWould you like to play again (Y/N)?: ").lower()
             print("\n-------------------------------------------------")
-            if choice.startswith('y' or 'Y'):
+            if choice == 'y' or choice == 'yes':
+                #break
                 shouldContinue2 = False
-                shouldContinue = False
-                shouldContinue = True
-            elif choice.startswith('n' or 'N'):
+            elif choice == 'n' or choice == 'no':
                 shouldContinue = False
                 shouldContinue2 = False
                 print("\nThanks for using my number guessing program.")
